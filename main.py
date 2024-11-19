a26869847a880d9de77163ac95d4bb363b959f35
JavaSiro <javokhirsirojiddinov@gmail.com>
2024-11-19 15:11:46 +0900

    get links

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import imaplib
 import email
 import os
+from bs4 import BeautifulSoup
 load_dotenv()
 username=os.getenv("EMAIL")
 password=os.getenv("PASSWORD")
@@ -10,10 +11,17 @@ def connect_to_mail():
     mail.login(username, password)
     mail.select("inbox")
     return mail
+
+def extraxt_links_from_html(html_content):
+    soup=BeautifulSoup(html_content, "html.parser")
+    links=[link["href"] for link in soup.find_all("a",href=True) if "unsubscribe" in link["href"].lower()]
+    return links 
 def search_for_email():
     mail=connect_to_mail()
     _,search_data=mail.search(None, '(BODY "unsubscribe")')
     data=search_data[0].split()
+    links= []
+
     for num in data:
         _, data = mail.fetch(num, "(RFC822)")
         msg = email.message_from_bytes(data[0][1])
@@ -21,11 +29,13 @@ def search_for_email():
             for part in msg.walk():
                 if part.get_content_type()=="text/html":
                     html_content=part.get_payload(decode=True).decode()
+                    links.extend(extraxt_links_from_html(html_content))
                     print(html_content)
         else:
             content_type=msg.get_content_type()
             content=msg.get_payload(decode=True).decode()
-            if content_type=="texy/html":
-                print(content)            
+            if content_type=="text/html":
+                links.extend(extraxt_links_from_html(content))
     mail.logout()
+    return links
     search_for_email()
